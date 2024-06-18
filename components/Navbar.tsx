f82f266be434341309e64d99a642b550ecde8dca
Your Name <you@example.com>
2024-06-18 15:19:53 +0530

    modified:   app/page.tsx     modified:   components/Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
 import { ToggleThemeButton } from "./ToggleThemeButton";
 
 export default async function Navbar() {
     return (
         <nav className="p-4 flex justify-between items-center sticky top-0">
-            <h1 className="text-3xl font-bold">Logo</h1>
+            <Link href="/">
+                <h1 className="text-3xl font-bold">Logo</h1>
+            </Link>
             <ToggleThemeButton />
         </nav>
     );
